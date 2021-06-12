38e55ffe4e553ac51153209e362ec062b62e0c39
dotnotation <dotinmotionproductions@gmail.com>
2021-06-12 14:16:43 -0400

    added Dog and DogCatcher class files

---
+++ lib/hairless_furever/DogCatcher.rb
@@ -0,0 +1,2 @@
+class DogCatcher
+end
--- lib/hairless_furever/cli.rb
+++ lib/hairless_furever/cli.rb
@@ -1,6 +1,6 @@
 class HairlessFurever::CLI
     def call
-        puts "Welcome to Hairless Furever"
+        puts "Welcome to Hairless Furever!"
         binding.pry
     end
 end
