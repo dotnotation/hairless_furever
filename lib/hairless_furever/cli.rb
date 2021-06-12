0eb051a81dd63080dfcc14e99847b6da0ed474a2
dotnotation <dotinmotionproductions@gmail.com>
2021-06-12 16:09:00 -0400

    commenting in tasks in CLI

--- lib/hairless_furever/cli.rb
+++ lib/hairless_furever/cli.rb
@@ -1,6 +1,11 @@
 class HairlessFurever::CLI
     def call
         puts "Welcome to Hairless Furever!"
-        binding.pry
+        puts "Please enter the number of the dog you would like more information on."
     end
+    #get dog breeds by index fetch_dog
+    #list the dog breeds fetched_dogs_list each.with_index(1)
+    #ask user which breed they would like more information on
+    #get user input
+    #give attributes of chosen dog
 end
